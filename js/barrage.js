a3f26d89264027b25d7fee537af8b4815f2a7c14
王名旭 <1220712534@qq.com>
2017-08-14 16:36:07 +0800

    init

---
+++ js/barrage.js
@@ -0,0 +1,210 @@
+/**
+ * Created by wmx on 2017/8/12.
+ */
+(function(global, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // Register as an anonymous AMD module:
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // Node/CommonJS:
+        factory(require('jquery'));
+    } else {
+        // Browser globals:
+        factory(window.jQuery);
+    }
+
+})(this, function($) {
+
+    var NAME = "Barrage",
+        VERSION = "1.0.0",
+        BARRAGE_KEY = 'barrage',
+        DATA_KEY = 'mx-barrage',
+        CONTAINER_CLASS = 'mx-barrage-container';
+    JQUERY_NO_CONFLICT = $.fn[NAME];
+
+    var Default = {
+        template: '<div class="mx-barrage">' + '<img class="mx-barrage-avatar"/>' + '<div class="mx-barrage-msg"></div>' + '</div>',
+        liOffset: {
+            top: 20,
+            bottom: 20
+        },
+        loop: true,
+        totalLine: 3,
+        // direction: 'left',
+        // liWidth: 200,
+        width: 'auto',
+        height: 'auto',
+        statusClass: {
+            success: 'mx-barrage-success',
+            warning: 'mx-barrage-warning',
+            danger: 'mx-barrage-danger',
+            normal: 'mx-barrage-normal'
+        },
+        status: 'danger',
+        loopTimerMax: 1500,
+        loopTimerMin: 1000,
+        runTimeMax: 4000,
+        runTimeMin: 3000
+    };
+
+    var Barrage = (function($) {
+        var Barrage = function(element, config) {
+            this._config = this._getConfig(config);
+            this._$element = $(element);
+            this._$element.data(BARRAGE_KEY, this);
+            this._$element.addClass(CONTAINER_CLASS);
+            this._isPlayed = false;
+            this._threadList = new Array(this._config.totalLine);
+            this._timerList = [];
+        };
+        Barrage.prototype.play = function(args) {
+            if (this._isPlayed) {
+                return this;
+            }
+            var barrageDataList = $.isArray(args[0])
+                ? args[0]
+                : [];
+            var barrageDataBackup = barrageDataList.slice(0);
+
+            config = args[1];
+            if (typeof config === "object") {
+                $.extend(this._config, config);
+            }
+            $.each(this._threadList, $.proxy(function(index, thread) {
+                var timer = setInterval($.proxy(function() {
+                    // debugger
+                    var barrageData,
+                        _tm;
+                    if (barrageDataList.length < 1) {
+                        if (this._config.loop) {
+                            barrageDataList = barrageDataBackup.slice(0);
+                        } else {
+                            return false;
+                        }
+                    }
+                    barrageData = barrageDataList.shift();
+                    _tm = new TipManager(this._config.template);
+                    _tm.text(barrageData.msg);
+                    _tm.setAvatar(barrageData.avatar);
+                    _tm._$tip.css('top', this._calcLineTop(index)).css('left', this._$element.width());
+                    if (barrageData.status) {
+                        _tm._$tip.addClass(this._config.statusClass[barrageData.status]);
+                    } else {
+                        _tm._$tip.addClass(this._config.statusClass[this._config.status]);
+                    }
+                    this._$element.append(_tm._$tip);
+                    this._runTip(_tm._$tip, thread);
+                }, this), this._getIntervalTime());
+                this._timerList.push(timer);
+            }, this));
+            this._isPlayed = true;
+            return this;
+
+        };
+        Barrage.prototype._getIntervalTime = function() {
+            var max = this._config.loopTimerMax;
+            var min = this._config.loopTimerMin;
+            return this._getRandomNum(max, min);
+        }
+        Barrage.prototype._getRunTime = function() {
+            var max = this._config.runTimeMax;
+            var min = this._config.runTimeMin;
+            return this._getRandomNum(max, min);
+        }
+        Barrage.prototype._getRandomNum = function(max, min) {
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        }
+        Barrage.prototype._calcLineTop = function(lineNo) {
+            var liOffset = this._config.liOffset;
+            var eleH = this._$element.height();
+            var lineHeight = (eleH - liOffset.top - liOffset.bottom) / this._config.totalLine;
+            return (lineHeight * lineNo + liOffset.top) + 'px';
+        };
+        Barrage.prototype._runTip = function(tip) {
+            $(tip).animate({
+                'left': '0px'
+            }, this._getRunTime(), 'linear', function() {
+                $(tip).remove();
+            });
+        };
+        Barrage.prototype.destroy = function() {
+            $.each(this._timerList, $.proxy(function(index, timer) {
+                clearInterval(timer);
+            }));
+            this._$element.removeData(BARRAGE_KEY).removeClass(CONTAINER_CLASS);
+            this._isPlayed = false;
+            delete this;
+        };
+        Barrage.prototype._getConfig = function(config) {
+            return $.extend(true, {}, Default, config);
+        };
+        Barrage._jQueryInterface = function(config) {
+            var args = [].slice.call(arguments, 1);
+            return this.each(function(i, elem) {
+
+                var data,
+                    _config,
+                    barrage;
+
+                data = $(elem).data(DATA_KEY);
+                _config = $.extend(true, {}, data, typeof config === 'object'
+                    ? config
+                    : null);
+                barrage = $(elem).data(BARRAGE_KEY);
+                if (!barrage) {
+                    barrage = new Barrage(elem, _config);
+                }
+                if (typeof config == "string") {
+                    barrage[config](args);
+                } else if (_config.play) {
+                    barrage.play(args);
+                }
+            })
+        };
+        return Barrage;
+
+    })($);
+
+    $.fn[NAME] = Barrage._jQueryInterface;
+    $.fn[NAME].Constructor = Barrage;
+    $.fn[NAME].noConflict = function() {
+        $.fn[NAME] = JQUERY_NO_CONFLICT;
+        return Barrage._jQueryInterface;
+    };
+    var TipManager = (function($) {
+
+        var SELECTOR = {
+            TEXT: ".mx-barrage-msg",
+            AVATAR: ".mx-barrage-avatar"
+        };
+        var TipManager = function(tip) {
+            this._$tip = $(tip);
+        };
+        TipManager.prototype.text = function(msg) {
+            if (typeof msg == 'string') {
+                this._setText(msg);
+                return this;
+            } else {
+                return this._getText();
+            }
+        };
+        TipManager.prototype.setAvatar = function(url) {
+            this.getAvatarElement().attr('src', url);
+        };
+        TipManager.prototype._setText = function(msg) {
+            this._$tip.find(SELECTOR.TEXT).html(msg);
+        };
+        TipManager.prototype._getText = function() {
+            return this._$tip.find(SELECTOR.TEXT).html();
+        };
+        TipManager.prototype.getContentElement = function() {
+            return this._$tip.find(SELECTOR.TEXT);
+        };
+        TipManager.prototype.getAvatarElement = function() {
+            return this._$tip.find(SELECTOR.AVATAR);
+        };
+
+        return TipManager;
+    })($);
+    typeof module == 'object' && (module.exports = Barrage);
+});
